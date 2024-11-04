071bb1ca4d632acdf8ab136bc5c02439cdb9f327
eealvarez <familia_alvarez@hotmail.com>
2024-11-03 22:51:36 -0600

    Rutas comodín

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
@@ -12,5 +13,17 @@ class VinylController
     {
         // die('Infunisa');
         return new Response('INFUNISA');
+    }
+
+    #[Route('/browse/{slug}', name: 'browse')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Esta es la paǵina de búsqueda: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'Todas las búsquedas';
+        }
+
+        return new Response($title);
     }
 }
